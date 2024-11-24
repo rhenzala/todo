7f5cc2d104bda1c7e4ac9f061a638dc0c972e7be
rhenzala <97076128+rhenzala@users.noreply.github.com>
2024-11-24 17:43:28 +0800

    Add button click handler for nav buttons
    Whenever a nav button is pressed, it will display a new button
    in the #container that will either add new todo or add new
    project, depending on the nav button.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
-import "./styles.css"
-import "./todo.js"
+import "./styles.css";
+import { compareAsc, format } from "date-fns";
+import { addTodoButton, addProjectButton } from "./dom_manipulation.js";
 
+const container = document.getElementById('container');
+let handleBtnClick = (event) => {
+    const buttonId = event.target.id; 
+    container.replaceChildren();
+    if (buttonId === "todo") {
+        addTodoButton(container);
+    } else if (buttonId === "project") {
+        addProjectButton(container);
+    } 
+}
+const buttons = document.querySelectorAll('.nav-buttons');
+buttons.forEach(btn =>{
+    btn.addEventListener('click', handleBtnClick)
+});
 
+document.addEventListener('DOMContentLoaded', () => {
+    addTodoButton(container);
+});
