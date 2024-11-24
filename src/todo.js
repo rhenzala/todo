e62a56a0df4f78a9a615533645eb9986b9624172
rhenzala <97076128+rhenzala@users.noreply.github.com>
2024-11-24 17:45:59 +0800

    Initial configuration

---
+++ src/todo.js
@@ -0,0 +1,10 @@
+import { compareAsc, format } from "date-fns";
+import "./dom_manipulation.js"
+
+const confirmBtn = document.getElementById('confirm');
+const addTask = () =>{
+    showTodoModal();
+    
+    const task = taskName.value.trim();
+
+}
