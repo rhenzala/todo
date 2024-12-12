2a5de02cd99c6f06314a60e12e987f89c249cbd7
rhenzala <97076128+rhenzala@users.noreply.github.com>
2024-12-12 12:38:27 +0800

    Add and configure tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+ 
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#f9fafb',
+      'black': '#030712',
+      'gray': '#e5e5e5',
+      'red': '#dc2626',
+      'amber': '#f59e0b',
+      'green': '#22c55e',
+    },
+    extend: {
+      gridTemplateColumns: {
+        'autofill': 'repeat(auto-fill, minmax(250px, 1fr))',
+      }
+    },  
+  },
+  plugins: [],
+}
+
