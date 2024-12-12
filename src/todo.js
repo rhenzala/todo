9e4ed50d9cd9075d9976465d0098428d529244eb
rhenzala <97076128+rhenzala@users.noreply.github.com>
2024-12-12 19:56:42 +0800

    Remove storage and card handler, revise others
    Since card handler and storage are in their own separate file now, for
    organization. Modified the invocation of storage handler and displayCard.

--- src/todo.js
+++ src/todo.js
@@ -1,12 +1,13 @@
-import { SingleProjectCard } from "./each_project";
-import { TodoModal } from "./dom_manipulation";
-import EditIcon from "../assets/edit.svg";
-import DeleteIcon from "../assets/delete.svg";
+import { myProjects } from "./project";
+import StorageHandler from "./storage";
+import displayCard from "./handle_card";
 
-export let myTasks = [];
+let myTasks = [];
+let projectTasks = [];
+
 const pageName = document.getElementById('pageName').textContent;
 
-export class Task {
+class Task {
     constructor(taskNameInput, descriptionInput, dateInput, priorityInput, assignedProject){
         this.taskName = taskNameInput;
         this.description = descriptionInput;
@@ -16,11 +17,11 @@ export class Task {
     }
     addTask(){
         myTasks.push(this);
-        StorageHandler.saveTask();
+        StorageHandler.saveTask(myTasks);
         if (pageName === "All Tasks"){
-            TodoCard.displayCard();
+            displayCard(myTasks, true, false, null, null);
         } else {
-            SingleProjectCard.displayCard(this.assignedProject);
+            displayCard(myTasks, true, true, projectTasks, this.assignedProject);
         }
     }
     updateDetails(newName, newDesc, newDueDate, newPriority) {
@@ -28,127 +29,51 @@ export class Task {
         this.description = newDesc;
         this.dueDate = newDueDate;
         this.priority = newPriority;
-        StorageHandler.saveTask();
+        StorageHandler.saveTask(myTasks);
         if (pageName === "All Tasks"){
-            TodoCard.displayCard();
+            displayCard(myTasks, true, false, null, null);
         } else {
-            SingleProjectCard.displayCard(this.assignedProject);
+            displayCard(myTasks, true, true, projectTasks, this.assignedProject);
         }
     }
 }
 
-export const TodoCard = (()=>{
-    const content = document.getElementById('content');
-    const _createCard = (task, index) => {
-        const card = document.createElement('div');
-        card.classList.add('card');
-        card.setAttribute('data-id', index);
-        const taskName = document.createElement('p');
-        const dueDate = document.createElement('p');
-        const description = document.createElement('p');
-        const assignedProject = document.createElement('p');
-        const cardBtn = document.createElement('div');
-        const editBtn = document.createElement('button');
-        const deleteBtn = document.createElement('button');
-        const editIcon = document.createElement('img');
-        editIcon.src = EditIcon;
-        editIcon.alt = "Edit";
-        const deleteIcon = document.createElement('img');
-        deleteIcon.src = DeleteIcon;
-        deleteIcon.alt = "Delete";
-        taskName.textContent = `${task.taskName}`;
-        dueDate.textContent = `${task.dueDate}`;
-        dueDate.classList.add('due-date');
-        description.textContent = `${task.description}`;
-        assignedProject.textContent = `${task.assignedProject}`;
-        cardBtn.classList.add('card-button');
-        editBtn.appendChild(editIcon);
-        editBtn.classList.add('edit-button');
-        deleteBtn.appendChild(deleteIcon);
-        deleteBtn.classList.add('delete-button');
 
-        _handlePriority(task.priority, card);
-        _handleEditBtn(editBtn, task, index)
-        _handleDeleteBtn(deleteBtn, index);
-        card.appendChild(taskName);
-        card.appendChild(dueDate);
-        card.appendChild(description);
-        card.appendChild(assignedProject);
-        cardBtn.appendChild(editBtn);
-        cardBtn.appendChild(deleteBtn);
-        card.appendChild(cardBtn);
-        content.appendChild(card);
+const HandleSingleProject = (() => {
+    let currentProjectName = "";
+
+    const getProjectDetails = (index, currentContext) => {
+        const project = myProjects[index];
+        if (project) {
+            currentContext = project.projectName;
+            currentProjectName = project.projectName;
+            const projTasks = _getAllProjectTasks(currentContext).length;
+            return [currentContext, projTasks];
+        }
+        return null;
     }
-    const _handleEditBtn = (btn, task, index) => {
-        btn.addEventListener('click', () => {
-            TodoModal.showEdit(task);
-            const editForm = document.getElementById('editTodoForm');
-            editForm.setAttribute('data-task-index', index);
-        });
+    const _getAllProjectTasks = (projectName) => {
+        projectTasks = myTasks.filter(task => task.assignedProject === projectName);
+        return projectTasks;
     }
-    const _handleDeleteBtn = (btn, index) => {
-        btn.addEventListener('click', () => {
-            myTasks.splice(index, 1);
-            StorageHandler.saveTask(); 
-            displayCard(); 
-        });
-    }
-    const _handlePriority = (priority, card) => {
-        if (priority === "high") {
-            card.id = "highPriority";
-        } else if (priority === "medium"){
-            card.id = "mediumPriority";
-        } else if (priority === "low"){
-            card.id = "lowPriority";
+    const getCurrentProject = () => currentProjectName;
+    const refreshProjectTasks = () => {
+        if (currentProjectName) {
+            _getAllProjectTasks(currentProjectName);
+            displayCard(myTasks, true, true, projectTasks, currentProjectName);
         }
     }
-    const displayCard = () => {
-        content.replaceChildren();
-        myTasks.forEach((task, index) =>{
-            _createCard(task, index);
-        })
-    }
+    const resetCurrentProjectName = () => currentProjectName = "";
     return{
-        displayCard
+        getProjectDetails,
+        getCurrentProject,
+        refreshProjectTasks,
+        resetCurrentProjectName
     }
 })();
 
-export const StorageHandler = (()=>{
-    const saveTask = () =>{
-        try {
-            localStorage.setItem('myTasks', JSON.stringify(myTasks));
-            console.log('Tasks saved successfully');
-        } catch (error) {
-            console.error('Error saving tasks:', error);
-        }
-    }
-    const loadTask = () => {
-        try {
-            const savedTasks = localStorage.getItem('myTasks');
-            if (savedTasks) {
-                const tasksData = JSON.parse(savedTasks);
-                myTasks = []; 
-                
-                tasksData.forEach(taskData => {
-                    const task = new Task(
-                        taskData.taskName,
-                        taskData.description,
-                        taskData.dueDate,
-                        taskData.priority,
-                        taskData.assignedProject
-                    );
-                    myTasks.push(task);
-                });
-                
-                console.log('Tasks loaded successfully');
-            }
-        } catch (error) {
-            console.error('Error loading tasks:', error);
-        }
-    };
+export { myTasks, Task, HandleSingleProject, projectTasks }
 
-    return {
-        saveTask,
-        loadTask,
-    };
-})();
+
+
+
